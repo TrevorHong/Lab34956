1d063a8b949efedb4e1b04fc12c08b3ec56e1e7b
TrevorHong <65141699+TrevorHong@users.noreply.github.com>
2023-10-11 16:34:57 -0700

    Added my work so far

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿namespace Lab3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try {
+                int classlabel = 1;
+                int predict = 3;
+                int datapoints = 100;
+
+
+                string[] attributes = new string[] {"damage", "pierce", "accuracy", "round passed"};
+
+                string[][] attributeValues = new string[attributes.Length][];  
+                attributeValues[0] = new string[] {"low", "medium", "high", ""};
+                attributeValues[1] = new string[] {"low", "medium", "high", ""};
+                attributeValues[2] = new string[] {"low", "medium", "high", ""};
+                attributeValues[3] = new string[] { "0", "1", "", "" };
+
+                for (int i = 0; i < predict + 1; ++i) {
+                    Console.Write("attribute values" + " " + attributes[i] + ": ");
+                    for (int j = 0; j < predict + 1; ++j)
+                    {
+                        Console.Write( attributeValues[i][j] + " ");
+                    }
+                    Console.WriteLine("\n");
+                }
+                  
+                Console.WriteLine("\n");
+            }
+            catch(Exception error) {
+                Console.WriteLine(error);
+
+            }
+        }
+    }
+}
